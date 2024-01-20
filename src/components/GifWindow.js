0a3360c2f2fec27ee0353bb5b39fcfea7a38a6bf
joshuasauter <joshua.sauter@duke.edu>
2024-01-19 20:45:13 -0500

    small adjustment to gif window

--- src/components/GifWindow.js
+++ src/components/GifWindow.js
@@ -3,7 +3,14 @@ import myGif from '../assets/logo512.gif';
 function GifWindow() {
     return (
       <div className="window gif-window">
-        <div className="window-titlebar">GIF Window</div>
+        <div className="window-titlebar">
+          <div className="titlebar-title"></div>
+          <div className="titlebar-buttons">
+            <span className="titlebar-button minimize-button"></span>
+            <span className="titlebar-button maximize-button"></span>
+            <span className="titlebar-button exit-button"></span>
+          </div>
+        </div>
         <img src={myGif} alt="Fun GIF" />
       </div>
     );
